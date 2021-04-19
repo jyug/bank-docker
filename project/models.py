8fe8faf832421b5b1c0de0045b0c7198e008795f
Jinfan <kimthebestman@gmail.com>
2021-04-19 14:17:07 -0700

    testing

--- project/models.py
+++ project/models.py
@@ -7,6 +7,7 @@ class Saving_Account(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     save_money = db.Column(db.Integer(20))
+    transaction = db.Column(db.String(100))
 
 
 class Checking_Account(db.Model):
