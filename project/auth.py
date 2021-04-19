2400d6e4f55a2d5d21fc98cfe4b3ee2459e71f78
Jinfan Zhang <62904466+jzhan253@users.noreply.github.com>
2021-04-19 13:03:54 -0700

    Update auth.py

--- project/auth.py
+++ project/auth.py
@@ -1,15 +1,40 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login')
+
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
-    return "<h1>login</h1>"
+    return render_template('login.html')
+
 
 @auth.route('/logout')
 def logout():
-    return "<h1>logout</h1>"
+    return render_template('logout.html')
 
-@auth.route('/sign-up')
+
+@auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
-    return "<h1>sign-up</h1>"
+    if request.method == 'POST':
+        email = request.form.get('email')
+        firstName = request.form.get('firstName')
+        lastName = request.form.get('lastName')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        if len(email) < 4:
+            flash('Email should be longer!', category='error')
+        elif len(firstName) < 2:
+            flash('First Name should be longer!', category='error')
+        elif len(lastName) < 2:
+            flash('Last Name should be longer!', category='error')
+        elif password1 != password2:
+            flash('Passwords not match', category='error')
+        elif len(password1) < 7:
+            flash('password should be longer!', category='error')
+        else:
+            # add user to database.
+            flash('Account created', category='success')
+
+
+    return render_template('signUp.html')
