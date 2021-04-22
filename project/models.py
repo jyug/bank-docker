79796a60eac70cdbd01b44c62eea0bb8ca81bd1b
Jinfan <kimthebestman@gmail.com>
2021-04-22 02:41:44 -0700

    improve user login

--- project/models.py
+++ project/models.py
@@ -24,6 +24,7 @@ class User(db.Model, UserMixin):
     last_name = db.Column(db.String(150))
     email = db.Column(db.String(150), unique=True)
     password = db.Column(db.String(150))
+    username = db.Column(db.String(150), unique=True)
 
 #
 #     def __init__(self, email, password, first_name, last_name):
