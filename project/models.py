c83ede5dc099331c8ca0c8b48defdb0284f8b553
Jinfan <kimthebestman@gmail.com>
2021-04-22 01:51:00 -0700

    finish database

--- project/models.py
+++ project/models.py
@@ -1,6 +1,9 @@
 # from project import db
-# from flask_login import UserMixin
-# from sqlalchemy.sql import func
+from flask_login import UserMixin
+from . import db
+from sqlalchemy.sql import func
+
+
 #
 #
 # class SavingAccount(db.Model):
@@ -15,15 +18,25 @@
 #     check_money = db.Column(db.Integer)
 #
 #
-# class User(db.Model, UserMixin):
-#     id = db.Column(db.Integer, primary_key=True)
-#     email = db.Column(db.String(150), unique=True)
-#     password = db.Column(db.String(150))
-#     first_name = db.Column(db.String(150))
-#     last_name = db.Column(db.String(150))
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(150))
+    last_name = db.Column(db.String(150))
+    email = db.Column(db.String(150), unique=True)
+    password = db.Column(db.String(150))
+
 #
 #     def __init__(self, email, password, first_name, last_name):
 #         self.email = email
 #         self.password = password
 #         self.first_name = first_name
 #         self.last_name = last_name
+
+
+# class User(UserMixin):
+#     def __init__(self, user_id, first_name, last_name, email, password):
+#         self.id = user_id
+#         self.first_name = first_name
+#         self.last_name = last_name
+#         self.email = email
+#         self.password = password
