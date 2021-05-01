ca01377c0e6849fc75056db2b49939c20a82f644
Jinfan <kimthebestman@gmail.com>
2021-05-01 16:38:53 -0700

    Wire transfer complete

--- project/auth.py
+++ project/auth.py
@@ -296,3 +296,55 @@ def internal_trans():
             flash('Successfully issued an internal money transfer!', category='success')
             return render_template('internal_trans.html', user=current_user)
     return render_template('internal_trans.html', user=current_user)
+
+
+@auth.route('/wire', methods=['GET', 'POST'])
+@login_required
+def wire():
+    if request.method == 'POST':
+        money = float(request.form.get('money'))
+        name = request.form.get('name')
+        firstName = name.split()[0]
+        lastName = name.split()[1]
+        email = request.form.get('email')
+        users = User.query.all()
+        source_idx = -1
+        for i in range(len(current_user.accounts)):
+            if current_user.accounts[i].type == 'checking':
+                source_idx = i
+        if source_idx not in [0, 1, 2]:
+            flash('You do not have a checking account yet, please contact the bank to open!', category='error')
+            return redirect(url_for('views.home'))
+        for user in users:
+            if user.email == email:
+                if user.first_name == firstName and user.last_name == lastName:
+                    for account in user.accounts:
+                        if account.type == 'checking':
+                            payment = Payment(amount=money,
+                                              target_id=account.id,
+                                              time=datetime.datetime.now())
+                            income = Income(amount=money,
+                                            source_id=current_user.accounts[source_idx].id,
+                                            time=datetime.datetime.now())
+                            current_user.accounts[source_idx].payments.append(payment)
+                            account.incomes.append(income)
+                            if money > current_user.accounts[source_idx].balance:
+                                flash('You do not have enough money!', category='error')
+                                return render_template('wire.html', user=current_user)
+                            else:
+                                current_user.accounts[source_idx].balance -= money
+                                account.balance += money
+                                db.session.add(payment)
+                                db.session.add(income)
+                                db.session.commit()
+                                flash('Wire transfer complete!', category='success')
+                                return redirect(url_for('views.home'))
+                    flash('The target user do not have a checking account yet, unable to issue the transfer!',
+                          category='error')
+                    return redirect(url_for('views.home'))
+                else:
+                    flash('Incorrect name! Not able to issue the wire transfer!', category='error')
+                    return render_template('wire.html', user=current_user)
+        flash('Email not found! No such user!', category='error')
+        return render_template('wire.html', user=current_user)
+    return render_template('wire.html', user=current_user)
