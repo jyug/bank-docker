bad2c79be58b3cb6b369cebe21a193ff8c0ea610
Cong Li <col041@ucsd.edu>
2021-05-13 13:04:51 -0700

    jsonify some api

--- project/auth.py
+++ project/auth.py
@@ -1,7 +1,8 @@
-from flask import Blueprint, render_template, request, flash, redirect, url_for
+from flask import Blueprint, render_template, request, flash, redirect, url_for, make_response, jsonify
 from .models import User, Account, Transaction
 from . import db
 from werkzeug.security import generate_password_hash
+from werkzeug import Response
 from flask_login import login_user, logout_user, current_user, login_required
 from random import randint
 import datetime
@@ -12,22 +13,35 @@ auth = Blueprint('auth', __name__)
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
-        login_pass = request.form.get('loginpass')
-        password = request.form.get('password')
+        if not request.is_json:
+            return make_response({'msg': "no json"}), 200
+
+        req = request.get_json()
+        login_pass = req.get('loginpass')
+        password = req.get('password')
+
         if check_email_username(login_pass):
             user = User.query.filter_by(username=login_pass).first()
         else:
             user = User.query.filter_by(email=login_pass).first()
+
         if user:
             if password == user.password:
+
                 flash('Welcome back ' + user.first_name + '!', category='success')
                 login_user(user, remember=True)
-                return redirect(url_for('views.home'))
+
+                return make_response(redirect(url_for('views.home'))), 302
+                # return redirect(url_for('views.home'))
             else:
                 flash('Invalid password, please try again!', category='error')
+                return make_response(jsonify({'msg': 'invalid password'}), 409)
         else:
             flash('Invalid email address, please try again!', category='error')
-    return render_template('login.html', user=current_user)
+            return make_response(jsonify({'msg': 'invalid email'}), 409)
+    else:
+        #return render_template('login.html', user=current_user)
+        return make_response(render_template('login.html', user=current_user))
 
 
 @auth.route('/logout')
@@ -40,24 +54,41 @@ def logout():
 @auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
     if request.method == 'POST':
-        username = request.form.get('username')
-        email = request.form.get('email')
-        firstName = request.form.get('firstName')
-        lastName = request.form.get('lastName')
-        password1 = request.form.get('password1')
-        password2 = request.form.get('password2')
+        if not request.is_json:
+            return make_response({'msg': "no json"}), 200
+
+        req = request.get_json()
+        username = req.get('username')
+        email = req.get('email')
+        firstName = req.get('firstName')
+        lastName = req.get('lastName')
+        password1 = req.get('password1')
+        password2 = req.get('password2')
+
+        # username = request.form.get('username')
+        # email = request.form.get('email')
+        # firstName = request.form.get('firstName')
+        # lastName = request.form.get('lastName')
+        # password1 = request.form.get('password1')
+        # password2 = request.form.get('password2')
+
         u1 = User.query.filter_by(username=username).first()
         u2 = User.query.filter_by(email=email).first()
         if u1:
             flash('Username has already been taken!', category='error')
+            return make_response(jsonify({'msg': 'invalid email taken'}), 409)
         elif u2:
             flash('Email has already been taken!', category='error')
+            return make_response(jsonify({'msg': 'invalid email taken'}), 409)
         elif len(email) < 4:
             flash('Email should be longer!', category='error')
+            return make_response(jsonify({'msg': 'invalid email longer'}), 409)
         elif password1 != password2:
             flash('Passwords not match', category='error')
+            return make_response(jsonify({'msg': 'invalid passwords not same input'}), 409)
         elif len(password1) < 7:
             flash('password should be longer!', category='error')
+            return make_response(jsonify({'msg': 'invalid password length input '}), 409)
         else:
             # add user to database.
             new_user = User(last_name=lastName, first_name=firstName, email=email, password=password2,
@@ -66,9 +97,11 @@ def sign_up():
             db.session.commit()
             login_user(new_user, remember=True)
             flash('Account created! Welcome ' + new_user.first_name + '!', category='success')
-            return redirect(url_for('views.home'))
-
-    return render_template('signUp.html', user=current_user)
+            return make_response(redirect(url_for('views.home'))), 302
+            # return redirect(url_for('views.home'))
+    else:
+        return make_response(render_template('signUp.html', user=current_user))
+    # return render_template('signUp.html', user=current_user)
 
 
 def check_email_username(s):
@@ -93,11 +126,20 @@ def user_info():
 @login_required
 def edit_user_info():
     if request.method == 'POST':
-        firstName = request.form.get('firstName')
-        lastName = request.form.get('lastName')
-        password1 = request.form.get('password1')
-        password2 = request.form.get('password2')
-        address = request.form.get('address')
+        if not request.is_json:
+            return make_response({'msg': "no json"}), 200
+
+        req = request.get_json()
+        address = req.get('address')
+        firstName = req.get('firstName')
+        lastName = req.get('lastName')
+        password1 = req.get('password1')
+        password2 = req.get('password2')
+        # firstName = request.form.get('firstName')
+        # lastName = request.form.get('lastName')
+        # password1 = request.form.get('password1')
+        # password2 = request.form.get('password2')
+        # address = request.form.get('address')
         if len(firstName) > 0:
             current_user.first_name = firstName
         if len(lastName) > 0:
@@ -105,16 +147,21 @@ def edit_user_info():
         if len(password1) > 0 and len(password2) > 0:
             if len(password1) < 7:
                 flash('password should be longer!', category='error')
+                return make_response(jsonify({'msg': 'invalid password length input '}), 409)
             elif password1 != password2:
                 flash('Passwords not match', category='error')
+                return make_response(jsonify({'msg': 'invalid password the same input '}), 409)
             else:
                 current_user.password = password2
         if len(address) > 0:
             current_user.address = address
         db.session.commit()
         flash('Information changed! Welcome back ' + current_user.first_name + '!', category='success')
-        return redirect(url_for('auth.user_info'))
-    return render_template('edit_user_info.html', user=current_user)
+        return make_response(redirect(url_for('auth.user_info'))), 302
+        # return redirect(url_for('auth.user_info'))
+    else:
+        return make_response(render_template('edit_user_info.html', user=current_user))
+        # return render_template('edit_user_info.html', user=current_user)
 
 
 @auth.route('/admin', methods=['GET', 'POST'])
