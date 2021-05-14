39a62af98432114e52c1af312946462745254c85
Cong Li <col041@ucsd.edu>
2021-05-14 00:27:14 -0700

    jsonify more api

--- project/auth.py
+++ project/auth.py
@@ -168,7 +168,15 @@ def edit_user_info():
 @login_required
 def admin():
     if request.method == 'POST':
+        # if not request.is_json:
+        #     return make_response({'msg': "no json"}), 200
+
         users = User.query.all()
+
+        # req = request.get_json()
+        # key = req.get('search')
+        # field = req.get('field')
+
         key = request.form.get('search')
         field = request.form.get('field')
         customers = []
@@ -191,8 +199,11 @@ def admin():
                     customers.append(user)
         if len(customers) == 0:
             found = 0
+        #return make_response(render_template('admin.html', user=current_user, customers=customers, found=found))
         return render_template('admin.html', user=current_user, customers=customers, found=found)
-    return render_template('admin.html', user=current_user)
+    else:
+        return render_template('admin.html', user=current_user)
+        # return make_response(render_template('admin.html', user=current_user))
 
 
 @auth.route('/account_management', methods=['GET', 'POST'])
@@ -202,7 +213,8 @@ def account_management():
         userid = int(request.form.get('id'))
         customer = User.query.get(userid)
         return render_template('account_management.html', user=current_user, customer=customer)
-    return render_template('admin.html', user=current_user)
+    else:
+        return render_template('admin.html', user=current_user)
 
 
 @auth.route('/open_account', methods=['GET', 'POST'])
@@ -373,12 +385,22 @@ def credit():
 @login_required
 def internal_trans():
     if request.method == 'POST':
-        money = float(request.form.get('money'))
+
+        if not request.is_json:
+            return make_response({'msg': "no json"}), 200
+        # money = float(request.form.get('money'))
+        req = request.get_json()
+        money = float(req.get('money'))
+
         if money <= 0:
             flash('Invalid money amount!', category='error')
-            return render_template('internal_trans.html', user=current_user)
-        source = request.form.get('source')
-        destination = request.form.get('destination')
+            # return render_template('internal_trans.html', user=current_user)
+            return make_response(render_template('internal_trans.html', user=current_user), 400)
+        #source = request.form.get('source')
+        #destination = request.form.get('destination')
+        source = req.get('source')
+        destination = req.get('destination')
+
         source_idx = -1
         destination_idx = -1
         for i in range(len(current_user.accounts)):
@@ -390,46 +412,71 @@ def internal_trans():
             if source_idx == -1:
                 flash('You do not have a ' + source + ' account yet, not able to issue transaction!',
                       category='error')
-                return render_template('internal_trans.html', user=current_user)
+                # return render_template('internal_trans.html', user=current_user)
+                return make_response(render_template('internal_trans.html', user=current_user), 400)
             if destination_idx == -1:
                 flash('You do not have a ' + destination + ' account yet, not able to issue transaction!',
                       category='error')
-                return render_template('internal_trans.html', user=current_user)
+                # return render_template('internal_trans.html', user=current_user)
+                return make_response(render_template('internal_trans.html', user=current_user), 400)
         if source_idx == destination_idx:
             flash('Can not transfer between same accounts!', category='error')
-            return render_template('internal_trans.html', user=current_user)
+            # return render_template('internal_trans.html', user=current_user)
+            return make_response(render_template('internal_trans.html', user=current_user), 400)
         else:
-            transaction = Transaction(type='internal', amount=money,
+            if money > current_user.accounts[source_idx].balance:
+                flash('You do not have enough money!', category='error')
+                return make_response(redirect(url_for('views.home')), 400)
+            else:
+                transaction = Transaction(type='internal', amount=money,
                                       target_id=current_user.accounts[destination_idx].id,
                                       source_id=current_user.accounts[source_idx].id,
                                       time=datetime.datetime.now(),
                                       description='Internal transfer')
 
-            current_user.accounts[source_idx].payments.append(transaction)
-            current_user.accounts[destination_idx].incomes.append(transaction)
-            current_user.accounts[source_idx].balance -= money
-            current_user.accounts[destination_idx].balance += money
-            db.session.add(transaction)
-            db.session.commit()
-            flash('Successfully issued an internal money transfer!', category='success')
-            return render_template('internal_trans.html', user=current_user)
-    return render_template('internal_trans.html', user=current_user)
+                current_user.accounts[source_idx].payments.append(transaction)
+                current_user.accounts[destination_idx].incomes.append(transaction)
+                current_user.accounts[source_idx].balance -= money
+                current_user.accounts[destination_idx].balance += money
+                db.session.add(transaction)
+                db.session.commit()
+                flash('Successfully issued an internal money transfer!', category='success')
+                return make_response(redirect(url_for('views.home')), 200)
+                # return render_template('internal_trans.html', user=current_user)
+                # return make_response(render_template('internal_trans.html', user=current_user), 200)
+    # return render_template('internal_trans.html', user=current_user)
+    return make_response(render_template('internal_trans.html', user=current_user), 200)
 
 
 @auth.route('/wire', methods=['GET', 'POST'])
 @login_required
 def wire():
     if request.method == 'POST':
-        money = float(request.form.get('money'))
+        # money = float(request.form.get('money'))
+
+        if not request.is_json:
+            return make_response({'msg': "no json"}), 200
+
+        req = request.get_json()
+        money = float(req.get('money'))
+
         if money <= 0:
             flash('Invalid money amount!', category='error')
-            return render_template('wire.html', user=current_user)
-        name = request.form.get('name')
+            #return render_template('wire.html', user=current_user)
+            return make_response(render_template('wire.html', user=current_user), 400)
+
+        # name = request.form.get('name')
+        # email = request.form.get('email')
+        # note = request.form.get('note')
+        # method = request.form.get('method')
+        name = req.get('name')
+        email = req.get('email')
+        note = req.get('note')
+        method = req.get('method')
+
         firstName = name.split()[0]
         lastName = name.split()[1]
-        email = request.form.get('email')
-        note = request.form.get('note')
-        method = request.form.get('method')
+
         users = User.query.all()
         source_idx = -1
         account_type = 'checking'
@@ -440,7 +487,9 @@ def wire():
                 source_idx = i
         if source_idx not in [0, 1, 2]:
             flash('You do not have a checking account yet, please contact the bank to open!', category='error')
-            return redirect(url_for('views.home'))
+
+            # return redirect(url_for('views.home'))
+            return make_response(redirect(url_for('views.home')), 400)
         for user in users:
             if user.email == email:
                 if user.first_name == firstName and user.last_name == lastName:
@@ -457,22 +506,28 @@ def wire():
 
                             if money > current_user.accounts[source_idx].balance:
                                 flash('You do not have enough money!', category='error')
-                                return render_template('wire.html', user=current_user)
+                                return make_response(redirect(url_for('views.home')), 400)
+                                # return render_template('wire.html', user=current_user)
                             else:
                                 current_user.accounts[source_idx].balance -= money
                                 account.balance += money
                                 db.session.add(transaction)
                                 db.session.commit()
                                 flash('Wire transfer complete!', category='success')
-                                return redirect(url_for('views.home'))
+                                return make_response(redirect(url_for('views.home')), 200)
+                                # return redirect(url_for('views.home'))
                     flash('The target user do not have a checking account yet, unable to issue the transfer!',
                           category='error')
                     return redirect(url_for('views.home'))
+                    #return make_response(redirect(url_for('views.home')), 400)
                 else:
                     flash('Incorrect name! Not able to issue the wire transfer!', category='error')
                     return render_template('wire.html', user=current_user)
+                    #return make_response(redirect(url_for('views.home')), 400)
         flash('Email not found! No such user!', category='error')
+        # return make_response(render_template('wire.html', user=current_user))
         return render_template('wire.html', user=current_user)
+    # return make_response(render_template('wire.html', user=current_user))
     return render_template('wire.html', user=current_user)
 
 
